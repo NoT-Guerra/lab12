df6c60477c7aaf9709e258912b8f232eb9f98815
Nicholas <nicholas.guerra@studio.unibo.it>
2024-12-09 12:16:48 +0100

    removed the unused import

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -3,7 +3,6 @@ package it.unibo.es1;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
